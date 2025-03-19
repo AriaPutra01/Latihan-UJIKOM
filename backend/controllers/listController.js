41fa41992d6bfb4f80e5fb135865d12614a918f8
Aria saputra <saputra13427@gmail.com>
2025-03-19 08:05:51 +0700

    fix: fix handle error next in list & taks controller

--- backend/controllers/listController.js
+++ backend/controllers/listController.js
@@ -1,26 +1,42 @@
 let self = {};
 const listService = require("../services/listService");
 
-self.index = async (req, res) => {
-  const lists = await listService.getAllList(req.user.id);
-  res.sendSuccess(lists, "List retrieved successfully!");
+self.index = async (req, res, next) => {
+  try {
+    const lists = await listService.getAllList(req.user.id);
+    res.sendSuccess(lists, "List retrieved successfully!");
+  } catch (error) {
+    next(error);
+  }
 };
 
-self.store = async (req, res) => {
-  const { name } = req.body;
-  const list = await listService.createList(req.user.id, name);
-  res.sendSuccess(list, "List created successfully!");
+self.store = async (req, res, next) => {
+  try {
+    const { name } = req.body;
+    const list = await listService.createList(req.user.id, name);
+    res.sendSuccess(list, "List created successfully!");
+  } catch (error) {
+    next(error);
+  }
 };
 
-self.update = async (req, res) => {
-  const { name } = req.body;
-  const list = await listService.updateList(req.params.id, name);
-  res.sendSuccess(list, "List updated successfully!");
+self.update = async (req, res, next) => {
+  try {
+    const { name } = req.body;
+    const list = await listService.updateList(req.params.id, name);
+    res.sendSuccess(list, "List updated successfully!");
+  } catch (error) {
+    next(error);
+  }
 };
 
-self.destroy = async (req, res) => {
-  const list = await listService.deleteList(req.params.id);
-  res.sendSuccess(list, "List deleted successfully!");
+self.destroy = async (req, res, next) => {
+  try {
+    const list = await listService.deleteList(req.params.id);
+    res.sendSuccess(list, "List deleted successfully!");
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = self;
--- backend/controllers/taksController.js
+++ backend/controllers/taksController.js
@@ -1,24 +1,40 @@
 let self = {};
 const taksService = require("../services/taksService");
 
-self.index = async (req, res) => {
-  const result = await taksService.getAllTaks(req.user.id);
-  res.sendSuccess(result, "Taks retrieved successfully!");
+self.index = async (req, res, next) => {
+  try {
+    const result = await taksService.getAllTaks(req.user.id);
+    res.sendSuccess(result, "Taks retrieved successfully!");
+  } catch (error) {
+    next(error);
+  }
 };
 
-self.store = async (req, res) => {
-  const result = await taksService.createTaks(req.body);
-  res.sendSuccess(result, "Taks created successfully!");
+self.store = async (req, res, next) => {
+  try {
+    const result = await taksService.createTaks(req.body);
+    res.sendSuccess(result, "Taks created successfully!");
+  } catch (error) {
+    next(error);
+  }
 };
 
-self.update = async (req, res) => {
-  const result = await taksService.updateTaks(req.params.id, req.body);
-  res.sendSuccess(result, "Taks updated successfully!");
+self.update = async (req, res, next) => {
+  try {
+    const result = await taksService.updateTaks(req.params.id, req.body);
+    res.sendSuccess(result, "Taks updated successfully!");
+  } catch (error) {
+    next(error);
+  }
 };
 
-self.destroy = async (req, res) => {
-  const result = await taksService.deleteTaks(req.params.id);
-  res.sendSuccess(result, "Taks deleted successfully!");
+self.destroy = async (req, res, next) => {
+  try {
+    const result = await taksService.deleteTaks(req.params.id);
+    res.sendSuccess(result, "Taks deleted successfully!");
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = self;
