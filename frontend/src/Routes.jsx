0a17131a1913ae799e93471da03f963d81c8e73a
Aria saputra <saputra13427@gmail.com>
2025-03-19 09:25:26 +0700

    feat: add register page FE

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router";
 import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 export default function RootRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Welcome />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router";
 
-export default function Login() {
+export default function Register() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background">
       <div className="w-full max-w-md space-y-6 p-8 rounded-2xl">
-        <h2 className="text-center text-2xl font-semibold text-white">Login</h2>
+        <h2 className="text-center text-2xl font-semibold text-white">
+          Register
+        </h2>
 
         <div>
           <label className="block text-sm font-medium text-white">Email</label>
           <input
             type="email"
-            defaultValue="ariasaptr134@gmail.com"
+            placeholder="Enter your email"
             className="w-full mt-1 p-3 text-white rounded-lg border border-white focus:outline-none focus:ring-2 focus:ring-primary"
           />
         </div>
@@ -22,13 +24,24 @@ export default function Login() {
           </label>
           <input
             type="password"
-            defaultValue="••••••••••••••"
+            placeholder="Enter your password"
+            className="w-full mt-1 p-3 text-white rounded-lg border border-white focus:outline-none focus:ring-2 focus:ring-primary"
+          />
+        </div>
+
+        <div>
+          <label className="block text-sm font-medium text-white">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            placeholder="Confirm your password"
             className="w-full mt-1 p-3 text-white rounded-lg border border-white focus:outline-none focus:ring-2 focus:ring-primary"
           />
         </div>
 
         <button className="w-full p-3 bg-primary text-white rounded-lg hover:bg-primary transition">
-          Login
+          Register
         </button>
 
         <div className="flex items-center justify-center gap-2 text-primary">
@@ -38,9 +51,9 @@ export default function Login() {
         </div>
 
         <p className="text-center text-white">
-          Don’t have an account?{" "}
-          <Link to="/register" className="text-primary hover:underline">
-            Register
+          Already have an account?{" "}
+          <Link to="/login" className="text-primary hover:underline">
+            Login
           </Link>
         </p>
       </div>
